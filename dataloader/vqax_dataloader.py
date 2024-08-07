d87d00ec525bb446bed7f1b8e3222e2373304f94
minhhoang2705 <tranhminh8464@gmail.com>
2024-08-07 21:47:30 +0700

    update VQA-X dataloader module

--- dataloader/vqax_dataloader.py
+++ dataloader/vqax_dataloader.py
@@ -1,214 +1,237 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.data
-from torch.utils.data import Dataset
-import torchvision.transforms as transforms
-from transformers import GPT2Tokenizer, AutoConfig
-from transformers import AdamW, get_linear_schedule_with_warmup
+import os
 import json
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 from PIL import Image
-from tqdm import tqdm
+from collections import Counter
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
 
+class VQADataset(Dataset):
+    """
+    A custom Dataset class for Visual Question Answering (VQA) tasks.
 
-class VQAXTrainDataset(Dataset):
+    This class processes and prepares data for VQA models, including images, questions, answers, and explanations.
 
-    def __init__(self, path, transform, tokenizer, max_seq_len):
+    Args:
+        data (dict): A dictionary containing the VQA data.
+        image_dir (str): Path to the directory containing images.
+        transform (callable, optional): A function/transform to apply to the images.
+        max_question_length (int, optional): Maximum length of questions. Defaults to 20.
+        max_explanation_length (int, optional): Maximum length of explanations. Defaults to 50.
+        max_vocab_size (int, optional): Maximum size of the vocabulary. Defaults to 10000.
+
+    Attributes:
+        data (dict): The VQA data.
+        image_dir (str): Path to the image directory.
+        transform (callable): Image transformation function.
+        question_ids (list): List of question IDs.
+        max_question_length (int): Maximum question length.
+        max_explanation_length (int): Maximum explanation length.
+        max_vocab_size (int): Maximum vocabulary size.
+        word2idx (dict): Mapping of words to indices.
+        idx2word (dict): Mapping of indices to words.
+        answer2idx (dict): Mapping of answers to indices.
+        idx2answer (dict): Mapping of indices to answers.
+    """
+    def __init__(self, data, image_dir, transform=None, max_question_length=20, max_explanation_length=50, max_vocab_size=10000):
+        self.data = data
+        self.image_dir = image_dir
+        self.transform = transform or transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        self.question_ids = list(self.data.keys())
+        self.max_question_length = max_question_length
+        self.max_explanation_length = max_explanation_length
+        self.max_vocab_size = max_vocab_size
         
-        self.tokenizer = tokenizer
-        self.transform = transform
-        self.max_seq_len = max_seq_len       # question + <bos> The answer is <answer> becase <explanation> <eos>
-        self.data = json.load(open(path, 'r'))
-        self.ids_list = list(self.data.keys())
+        self.word2idx = {'<PAD>': 0, '<UNK>': 1, '<START>': 2, '<END>': 3}
+        self.idx2word = {0: '<PAD>', 1: '<UNK>', 2: '<START>', 3: '<END>'}
+        self.answer2idx = {}
+        self.idx2answer = {}
         
-        for k,v in self.data.items():   
-            if len(v['explanation']) > 1:   # some questions have more than one explanation
-                # duplicate them for loading. -1 because one explanation is already in ids_list
-                self.ids_list += [str(k)] * (len(v['explanation']) - 1)    
-
-        self.index_tracker = {k: len(v['explanation']) - 1 for k,v in self.data.items()}
+        self.build_vocab()
+        self.build_answer_vocab()
         
-
-    def __getitem__(self, i):
+    def build_vocab(self):
+        """
+        Builds the vocabulary from questions and explanations in the dataset.
+        """
+        word_freq = Counter()
+        for item in self.data.values():
+            word_freq.update(word_tokenize(item['question'].lower()))
+            word_freq.update(word_tokenize(item.get('explanation', [''])[0].lower()))
         
-        quention_id = self.ids_list[i]
-        sample = self.data[quention_id]
-        img_name = sample['image_name']
-        text_a = data_utils.proc_ques(sample['question'])    # question
-        answer = data_utils.proc_ans(sample['answers'])
-
-        exp_idx = self.index_tracker[quention_id]    # the index of the explanation for questions with multiple explanations
-        if exp_idx > 0:
-            self.index_tracker[quention_id] -= 1    # decrease usage
-                
-        text_b = sample['explanation'][exp_idx]   # explanation
-        
-        # tokenization process
-        q_segment_id, a_segment_id, e_segment_id = self.tokenizer.convert_tokens_to_ids(['<question>', 
-                                                                                         '<answer>', 
-                                                                                         '<explanation>'])
-        tokens = self.tokenizer.tokenize(text_a)
-        labels = [-100] * len(tokens)   # we dont want to predict the question, set to pad to ignore in XE
-        segment_ids = [q_segment_id] * len(tokens)
-
-        answer = [self.tokenizer.bos_token] + self.tokenizer.tokenize(" the answer is " + answer)
-        answer_len = len(answer)
-        tokens_b = self.tokenizer.tokenize(" because " + text_b) + [self.tokenizer.eos_token]
-        exp_len = len(tokens_b)
-        tokens += answer + tokens_b
-        labels += [-100] + answer[1:] + tokens_b   # labels will be shifted in the model, so for now set them same as tokens
-        segment_ids += [a_segment_id] * answer_len
-        segment_ids += [e_segment_id] * exp_len
-
-        if len(tokens) > self.max_seq_len :
-            tokens = tokens[:self.max_seq_len]
-            labels = labels[:self.max_seq_len]
-            segment_ids = segment_ids[:self.max_seq_len]
-
-
-        assert len(tokens) == len(segment_ids) 
-        assert len(tokens) == len(labels)
-        
-        seq_len = len(tokens)
-        padding_len = self.max_seq_len - seq_len
-        tokens = tokens + ([self.tokenizer.pad_token] * padding_len)
-        labels = labels + ([-100] * padding_len)
-        
-        segment_ids += ([e_segment_id] * padding_len)
-        input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
-        input_ids = torch.tensor(input_ids, dtype=torch.long)
-
-        labels = [self.tokenizer.convert_tokens_to_ids(t) if t!=-100 else t for t in labels]
-        labels = torch.tensor(labels, dtype=torch.long)
-        
-        segment_ids = torch.tensor(segment_ids, dtype=torch.long)
-        
-        folder = 'images/train2014/' if 'train' in img_name else 'images/val2014/' 
-        img_path = folder + img_name
-        img = Image.open(img_path).convert('RGB')
-        img = self.transform(img)
-        qid = torch.LongTensor([int(quention_id)])
-        
-        return (img, qid, input_ids, labels, segment_ids)
-
-    def __len__(self):
-        return len(self.ids_list)
+        for word, _ in word_freq.most_common(self.max_vocab_size - 4):  # -4 for <PAD>, <UNK>, <START>, <END>
+            idx = len(self.word2idx)
+            self.word2idx[word] = idx
+            self.idx2word[idx] = word
     
-class VQAXEvalDataset(Dataset):
-
-    def __init__(self, path, transform, tokenizer, max_seq_len):
-
-        self.tokenizer = tokenizer
-        self.transform = transform
-        self.max_seq_len = max_seq_len       # question + <bos> The answer is <answer> becase <explanation> <eos>
-        self.data = json.load(open(path, 'r'))
-        self.ids_list = list(self.data.keys())
-
-
-    def __getitem__(self, i):
+    def build_answer_vocab(self):
+        """
+        Builds the answer vocabulary from the dataset.
+        """
+        answer_freq = Counter()
+        for item in self.data.values():
+            answers = [ans['answer'].lower() for ans in item['answers']]
+            answer_freq.update(answers)
         
-        quention_id = self.ids_list[i]
-        sample = self.data[quention_id]
-        img_name = sample['image_name']
-        text_a = data_utils.proc_ques(sample['question'])    # question
-
-        # tokenization process
-        q_segment_id, a_segment_id, e_segment_id = self.tokenizer.convert_tokens_to_ids(['<question>', '<answer>', '<explanation>'])
-        tokens = self.tokenizer.tokenize(text_a)
-        segment_ids = [q_segment_id] * len(tokens)
-
-        answer = [self.tokenizer.bos_token] + self.tokenizer.tokenize(" the answer is")
-        answer_len = len(answer)
-        tokens += answer 
-
-        segment_ids += [a_segment_id] * answer_len
-
-        input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
-        input_ids = torch.tensor(input_ids, dtype=torch.long)
-        segment_ids = torch.tensor(segment_ids, dtype=torch.long)
-        
-        folder = 'images/train2014/' if 'train' in img_name else 'images/val2014/'   # test and val are both in val2014
-        img_path = folder + img_name
-        img = Image.open(img_path).convert('RGB')
-        img = self.transform(img)
-        qid = torch.LongTensor([int(quention_id)])
-        
-        return (img, qid, input_ids, segment_ids)
-
-    def __len__(self):
-        return len(self.ids_list)
-
-
-def sample_sequences(model, tokenizer, loader):
+        for answer, _ in answer_freq.most_common():
+            idx = len(self.answer2idx)
+            self.answer2idx[answer] = idx
+            self.idx2answer[idx] = answer
     
-    model.eval()
-    results_exp = []
-    results_full = []
-    SPECIAL_TOKENS = ['<|endoftext|>', '<pad>', '<question>', '<answer>', '<explanation>']
-    special_tokens_ids = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS)
-    because_token = tokenizer.convert_tokens_to_ids('Ġbecause')
-    max_len = 20
+    def tokenize(self, text):
+        """
+        Tokenizes and converts text to token indices.
+
+        Args:
+            text (str): Input text to tokenize.
+
+        Returns:
+            list: List of token indices.
+        """
+        tokens = word_tokenize(text.lower())
+        return [self.word2idx.get(token, self.word2idx['<UNK>']) for token in tokens]
     
-    for i,batch in enumerate(loader):
-        
-        current_output = []
-        batch = tuple(input_tensor.to(device) for input_tensor in batch)
-        img, img_id, input_ids, segment_ids = batch
-        img_embeddings = image_encoder(img)
-        always_exp = False
-        
-        with torch.no_grad():
-            
-            for step in range(max_len + 1):
-                
-                if step == max_len:
-                    break
-                
-                outputs = model(input_ids=input_ids, 
-                                past_key_values=None, 
-                                attention_mask=None, 
-                                token_type_ids=segment_ids, 
-                                position_ids=None, 
-                                encoder_hidden_states=img_embeddings, 
-                                encoder_attention_mask=None, 
-                                labels=None, 
-                                use_cache=False, 
-                                return_dict=True)
-                
-                lm_logits = outputs.logits 
-                logits = lm_logits[0, -1, :] / temperature
-                logits = top_filtering(logits, top_k=top_k, top_p=top_p)
-                probs = F.softmax(logits, dim=-1)
-                prev = torch.topk(probs, 1)[1] if no_sample else torch.multinomial(probs, 1)
-                
-                if prev.item() in special_tokens_ids:
-                    break
-                
-                # take care of when to start the <explanation> token
-                if not always_exp:
-                    
-                    if prev.item() != because_token:
-                        new_segment = special_tokens_ids[-2]   # answer segment
-                    else:
-                        new_segment = special_tokens_ids[-1]   # explanation segment
-                        always_exp = True
-                else:
-                    new_segment = special_tokens_ids[-1]   # explanation segment
-                    
-                new_segment = torch.LongTensor([new_segment]).to(device)
-                current_output.append(prev.item())
-                input_ids = torch.cat((input_ids, prev.unsqueeze(0)), dim = 1)
-                segment_ids = torch.cat((segment_ids, new_segment.unsqueeze(0)), dim = 1)
-                
-        decoded_sequences = tokenizer.decode(current_output, skip_special_tokens=True).lstrip()
-        results_full.append({"image_id": img_id.item(), "caption": decoded_sequences})
-        
-        if 'because' in decoded_sequences:
-            cut_decoded_sequences = decoded_sequences.split('because')[-1].strip()
+    def pad_sequence(self, sequence, max_length):
+        """
+        Pads or truncates a sequence to a fixed length.
+
+        Args:
+            sequence (list): Input sequence.
+            max_length (int): Desired length of the sequence.
+
+        Returns:
+            list: Padded or truncated sequence.
+        """
+        if len(sequence) > max_length:
+            return sequence[:max_length]
         else:
-            cut_decoded_sequences = " ".join(decoded_sequences.split()[2:])
+            return sequence + [self.word2idx['<PAD>']] * (max_length - len(sequence))
+    
+    def __len__(self):
+        """
+        Returns the number of items in the dataset.
+
+        Returns:
+            int: Number of items in the dataset.
+        """
+        return len(self.question_ids)
+    
+    def __getitem__(self, idx):
+        """
+        Retrieves an item from the dataset.
+
+        Args:
+            idx (int): Index of the item to retrieve.
+
+        Returns:
+            dict: A dictionary containing the processed image, question, answer, explanation, and question ID.
+        """
+        question_id = self.question_ids[idx]
+        item = self.data[question_id]
         
-        results_exp.append({"image_id": img_id.item(), "caption": cut_decoded_sequences})
-        print("\rEvaluation: Finished {}/{}".format(i, len(loader)), end='          ')
-            
-    return results_full, results_exp
+        # Load and process image
+        image_path = os.path.join(self.image_dir, item['image_name'])
+        image = Image.open(image_path).convert('RGB')
+        image = self.transform(image)
+        
+        # Process question
+        question = self.tokenize(item['question'])
+        question = self.pad_sequence(question, self.max_question_length)
+        question = torch.LongTensor(question)
+        
+        # Process answers
+        answers = [ans['answer'].lower() for ans in item['answers']]
+        answer_count = Counter(answers)
+        most_common_answer = answer_count.most_common(1)[0][0]
+        answer = self.answer2idx.get(most_common_answer, 0)
+        
+        # Process explanation
+        explanation = item.get('explanation', [''])[0]
+        explanation = self.tokenize(explanation)
+        explanation = [self.word2idx['<START>']] + explanation + [self.word2idx['<END>']]
+        explanation = self.pad_sequence(explanation, self.max_explanation_length)
+        explanation = torch.LongTensor(explanation)
+        
+        return {
+            'image': image,
+            'question': question,
+            'answer': answer,
+            'explanation': explanation,
+            'question_id': question_id
+        }
+
+def load_data(path):
+    with open(path, "r") as f:
+        data = json.load(f)
+    return data
+
+def process_data(train_path, val_path, test_path, train_image_dir, val_image_dir, test_image_dir, batch_size=512, num_workers=4):
+    """
+    Processes VQA data and creates DataLoader objects for training, validation, and testing.
+
+    Args:
+        train_path (str): Path to the training data JSON file.
+        val_path (str): Path to the validation data JSON file.
+        test_path (str): Path to the test data JSON file.
+        train_image_dir (str): Path to the directory containing training images.
+        val_image_dir (str): Path to the directory containing validation images.
+        test_image_dir (str): Path to the directory containing test images.
+        batch_size (int, optional): Batch size for DataLoaders. Defaults to 512.
+        num_workers (int, optional): Number of worker processes for DataLoaders. Defaults to 4.
+
+    Returns:
+        tuple: A tuple containing:
+            - train_loader (DataLoader): DataLoader for the training set.
+            - val_loader (DataLoader): DataLoader for the validation set.
+            - test_loader (DataLoader): DataLoader for the test set.
+            - word2idx (dict): Mapping of words to indices.
+            - answer2idx (dict): Mapping of answers to indices.
+            - idx2word (dict): Mapping of indices to words.
+            - idx2answer (dict): Mapping of indices to answers.
+    """
+    # Load datasets
+    train_data = load_data(train_path)
+    val_data = load_data(val_path)
+    test_data = load_data(test_path)
+    
+    # Create custom dataset classes
+    train_dataset = VQADataset(train_data, train_image_dir)
+    val_dataset = VQADataset(val_data, val_image_dir)
+    test_dataset = VQADataset(test_data, test_image_dir)
+    
+    # Create data loaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    
+    return train_loader, val_loader, test_loader, train_dataset.word2idx, train_dataset.answer2idx, train_dataset.idx2word, train_dataset.idx2answer
+
+if __name__ == "__main__":
+    # Example usage
+    # File paths
+    train_path = "/home/VLAI/datasets/VQA-X/vqaX_train.json"
+    val_path = "/home/VLAI/datasets/VQA-X/vqaX_val.json"
+    test_path = "/home/VLAI/datasets/VQA-X/vqaX_test.json"
+    train_image_dir = '/home/VLAI/datasets/COCO_Images/train2014'
+    val_image_dir = '/home/VLAI/datasets/COCO_Images/val2014'
+    test_image_dir = '/home/VLAI/datasets/COCO_Images/val2014'
+
+    # Process data and create data loaders
+    train_loader, val_loader, test_loader, word2idx, answer2idx, idx2word, idx2answer = process_data(
+        train_path, val_path, test_path, train_image_dir, val_image_dir, test_image_dir)
+
+    # iterating through the data loader
+    for batch in train_loader:
+        print("Batch of images:", batch['image'].shape)
+        print("Batch of questions:", batch['question'].shape)
+        print("Batch of answers:", batch['answer'].shape)
+        print("Batch of explanations:", batch['explanation'].shape)
+        print("Batch of question IDs:", batch['question_id'])
+        break
